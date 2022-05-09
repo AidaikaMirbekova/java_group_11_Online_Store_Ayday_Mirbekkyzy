88e041a61d1d073a475f3d341b6c471122bc9f2b
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-05-09 09:45:06 +0600

    Lesson 65 (commit 6) add freemarker file fpr show products list

--- src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Controller/ProductsController.java
+++ src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Controller/ProductsController.java
@@ -3,7 +3,7 @@ package com.example.java_group_11_online_store_ayday_mirbekkyzy.Controller;
 import com.example.java_group_11_online_store_ayday_mirbekkyzy.DTO.ProductsDTO;
 import com.example.java_group_11_online_store_ayday_mirbekkyzy.Repository.ProductsRepository;
 import com.example.java_group_11_online_store_ayday_mirbekkyzy.Service.ProductsService;
-import org.springframework.data.domain.Page;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class ProductsController {
     public final ProductsService productsService;
-    public final ProductsRepository productsRepository;
+    private final ModelMapper mapper = new ModelMapper();
 
-    public ProductsController(ProductsService productsService, ProductsRepository productsRepository) {
+    public ProductsController(ProductsService productsService) {
         this.productsService = productsService;
-        this.productsRepository = productsRepository;
+
     }
 
     @GetMapping
