2b795be9d071710b796f59509c6414f3a1c14af9
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-05-21 16:12:15 +0600

    Lesson 68 (commit 4) add changes in SecurityCongiguration for login

--- src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Confoguration/SecurityCongiguration.java
+++ src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Confoguration/SecurityCongiguration.java
@@ -1,7 +1,9 @@
 package com.example.java_group_11_online_store_ayday_mirbekkyzy.Confoguration;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,9 +11,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityCongiguration extends WebSecurityConfigurerAdapter {
+    private final DataSource dataSource;
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
@@ -19,24 +25,38 @@ public class SecurityCongiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/api/**")
-                .fullyAuthenticated();
+        http.csrf();
+        http.formLogin()
+                .loginPage("/login")
+                .failureUrl("/?error=true");
+
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true);
 
         http.authorizeRequests()
-                .antMatchers("/login")
-                .fullyAuthenticated();
+                .antMatchers("/profile")
+                .authenticated();
 
         http.authorizeRequests()
                 .anyRequest()
                 .permitAll();
+    }
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth ) throws Exception {
+            String fetchUsersQuery = "select email, password, enabled"
+                    + " from user_table"
+                    + " where email = ?";
 
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            String fetchRolesQuery = "select email, role"
+                    + " from user_table"
+                    + " where email = ?";
+           auth.jdbcAuthentication()
+                   .dataSource(dataSource)
+                   .usersByUsernameQuery(fetchUsersQuery)
+                   .authoritiesByUsernameQuery(fetchRolesQuery);
 
-        http.httpBasic();
-
-        http.formLogin().disable().logout().disable();
-
-        http.csrf().disable();
     }
 }
