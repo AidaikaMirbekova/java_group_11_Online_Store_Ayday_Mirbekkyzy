6b115580afc0341b5738e4b11fec1232fe285ced
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-05-21 16:09:03 +0600

    Lesson 68 (commit 2) add exceptions for method login and register user

---
+++ src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.java_group_11_online_store_ayday_mirbekkyzy.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException{
+}
