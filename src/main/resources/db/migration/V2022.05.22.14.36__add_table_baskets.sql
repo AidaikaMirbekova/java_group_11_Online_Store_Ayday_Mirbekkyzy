1a15bec71b00e4b171876a57e8077fb3a4096fe2
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-05-23 00:09:23 +0600

    Lesson 69 (commit 5) add validation file for database by create table busket

---
+++ src/main/resources/db/migration/V2022.05.22.14.36__add_table_baskets.sql
@@ -0,0 +1,11 @@
+
+create table `baskets`(
+                         `id` int not null auto_increment,
+                         `customers_id` int(10),
+                         `products_id` int(10),
+                         `quantity` int(10),
+                         `price` float(10),
+                         primary key (`id`),
+                         constraint `fk_customer_busket` foreign key (`customers_id`) references `user_table` (`id`),
+                         constraint `fk_products_basket` foreign key (`products_id`) references `products` (`id`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
