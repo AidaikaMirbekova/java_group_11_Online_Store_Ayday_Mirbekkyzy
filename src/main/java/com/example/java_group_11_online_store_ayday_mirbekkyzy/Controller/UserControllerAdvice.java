d36b46fe6669a325833d485c446177d873f4e12e
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-05-28 01:02:47 +0600

    Lesson 65 (commit 2) add controller advice class for catch exception

--- src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Controller/UserControllerAdvice.java
+++ src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Controller/UserControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.example.java_group_11_online_store_ayday_mirbekkyzy.Controller;
 
+import com.example.java_group_11_online_store_ayday_mirbekkyzy.Exception.UserAlreadyRegister;
 import com.example.java_group_11_online_store_ayday_mirbekkyzy.Exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
@@ -17,7 +18,16 @@ public class UserControllerAdvice {
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     private String userNotFound(UserNotFoundException ex, Model model){
-        model.addAttribute("ex", ex.toString());
+        model.addAttribute("recource", ex.getResource());
+        model.addAttribute("email",ex.getEmail());
+        return "user-not-found";
+    }
+
+    @ExceptionHandler(UserAlreadyRegister.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    private String userExistRegister(UserAlreadyRegister ex, Model model){
+        model.addAttribute("recource", ex.getResource());
+        model.addAttribute("email",ex.getEmail());
         return "user-not-found";
     }
 }
