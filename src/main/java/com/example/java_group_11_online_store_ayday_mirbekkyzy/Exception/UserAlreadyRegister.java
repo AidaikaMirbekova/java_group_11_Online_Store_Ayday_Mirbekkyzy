0edbf914e8a336bb5797bcc58125ed0b63cbb112
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-05-28 01:02:13 +0600

    Lesson 65 (commit 2) add exception class

--- src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Exception/UserAlreadyRegister.java
+++ src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Exception/UserAlreadyRegister.java
@@ -1,8 +1,19 @@
 package com.example.java_group_11_online_store_ayday_mirbekkyzy.Exception;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.CONFLICT)
+@Getter @Setter
 public class UserAlreadyRegister extends RuntimeException{
+    private String resource;
+   private String email;
+
+    public UserAlreadyRegister(String resource, String email) {
+        super();
+        this.resource = resource;
+        this.email = email;
+    }
 }
--- src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Exception/UserNotFoundException.java
+++ src/main/java/com/example/java_group_11_online_store_ayday_mirbekkyzy/Exception/UserNotFoundException.java
@@ -1,11 +1,18 @@
 package com.example.java_group_11_online_store_ayday_mirbekkyzy.Exception;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
+@Getter @Setter
 public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String s) {
+    private String resource;
+    private String email;
 
+    public UserNotFoundException(String resource, String email) {
+        this.resource = resource;
+        this.email = email;
     }
 }
